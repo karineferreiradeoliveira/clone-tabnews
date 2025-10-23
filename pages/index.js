f0584b0ee76e7e31cbb6085fd28efefc6b50fe8b
Karine Ferreira <30732416+karineferreiradeoliveira@users.noreply.github.com>
2025-10-23 19:34:05 +0000

    Alterando conteúdo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1> Liz, eu te amo. Dá uma risadinha de gostou! </h1>
+    return <h1> Liz, eu te amo. Dá uma risadinha se gostou!
+         </h1>
 }
 
 export default Home;
