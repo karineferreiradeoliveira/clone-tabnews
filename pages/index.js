ce344e611af0b990ac4460b8bb0f747d2e340650
Karine Ferreira <30732416+karineferreiradeoliveira@users.noreply.github.com>
2025-10-24 19:42:45 +0000

    Adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
-    return <h1> Liz, eu te amo. Dá uma risadinha se gostou!
-         </h1>
+  return <h1> Liz, eu te amo. Dá uma risadinha se gostou!</h1>;
 }
 
+function teste() {
+  console.log("eeiiiii");
+}
 export default Home;
