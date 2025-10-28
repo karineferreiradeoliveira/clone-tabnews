6e527f05f65ed3c2c94f57ec635a46cecbc54ecd
Karine Ferreira <30732416+karineferreiradeoliveira@users.noreply.github.com>
2025-10-28 17:02:01 +0000

    Organizando div conteúdo

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,36 @@
 function Home() {
   return (
     <>
-      <h1> Liz, eu te amo. Dá uma risadinha se gostou!</h1>;
-      <h1>knotes.com.br: O Diário de Bordo da Minha Evolução Dev.</h1>
-      <h2>Bem-vindo(a) ao K-Notes.</h2>
-      <p>
-        Este espaço é o meu kernel de aprendizados, descobertas e curiosidades
-        do universo de desenvolvimento de software. Se você está aqui, é
-        provável que compartilhe a mesma sensação de pura diversão e entusiasmo
-        que encontrei ao mergulhar nos estudos do curso.dev.{" "}
-        <p>
-          O que você encontra aqui? Mais do que tutoriais, o K-Notes é a minha
-          Jornada de Pensamento documentada. São anotações, links interessantes,
-          insights técnicos e, principalmente, a materialização da minha
-          curiosidade em código. É onde o "Aha!" de um novo conceito ganha forma
-          escrita.
-        </p>{" "}
-        <p>
-          {" "}
-          Por que documentar? Acredito que o aprendizado só se solidifica quando
-          o colocamos para fora. Este é o meu jeito de estruturar o caos da
-          informação e, quem sabe, facilitar a sua jornada também. Sinta-se à
-          vontade para acompanhar, aprender junto e se inspirar.{" "}
-        </p>
-        <p>O futuro é construído com anotações de hoje.</p>
-      </p>
+      <div>
+        <h1> Liz, eu te amo. Dá uma risadinha se gostou!</h1>;
+        <h1>knotes.com.br: O Diário de Bordo da Minha Evolução Dev.</h1>
+        <h2>Bem-vindo(a) ao K-Notes.</h2>
+        <div>
+          <p>
+            Este espaço é o meu kernel de aprendizados, descobertas e
+            curiosidades do universo de desenvolvimento de software. Se você
+            está aqui, é provável que compartilhe a mesma sensação de pura
+            diversão e entusiasmo que encontrei ao mergulhar nos estudos do
+            curso.dev.{" "}
+            <p>
+              O que você encontra aqui? Mais do que tutoriais, o K-Notes é a
+              minha Jornada de Pensamento documentada. São anotações, links
+              interessantes, insights técnicos e, principalmente, a
+              materialização da minha curiosidade em código. É onde o "Aha!" de
+              um novo conceito ganha forma escrita.
+            </p>{" "}
+            <p>
+              {" "}
+              Por que documentar? Acredito que o aprendizado só se solidifica
+              quando o colocamos para fora. Este é o meu jeito de estruturar o
+              caos da informação e, quem sabe, facilitar a sua jornada também.
+              Sinta-se à vontade para acompanhar, aprender junto e se
+              inspirar.{" "}
+            </p>
+            <p>O futuro é construído com anotações de hoje.</p>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
