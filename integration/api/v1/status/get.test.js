0689aeb6e581801bbe9ca2c051e36859b4bf5480
Karine Ferreira <30732416+karineferreiradeoliveira@users.noreply.github.com>
2025-11-20 20:33:29 +0000

    Adicionando database.js

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
 test("Isso é 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log("oie");
+
+  // 👇 ISSO é a asserção de verdade:
+  expect(response.status).toBe(200);
+
+  const body = await response.json();
+  expect(body).toEqual({ chave: "Acima da média" });
 });
